2c9f79144dfdc536472d19b5960c12f689ca2ca7
雷云 <lei_yun0606@126.com>
2018-12-04 14:12:29 +0800

    first commit

---
+++ pages/index/index.js
@@ -0,0 +1,173 @@
+//index.js
+const app = getApp()
+
+Page({
+  data: {
+    indicatorDots: true,
+    swiperList: ['leiyun', 'leibaobao', 'daleizi'],
+    interval:1000,
+    autoplay: true,
+    circular: true,
+    indicatorColor: "red",
+    shoplistList: [
+      {
+        span: '苹果',
+        img:'../../images/shop.jpg',
+        url: '../apple/apple'
+      },
+      {
+        span: '香蕉',
+        img: '../../images/shop.jpg',
+        url: '../apple/apple'
+      },
+      {
+        span: '橙子',
+        img: '../../images/shop.jpg',
+        url: '../apple/apple'
+      },
+      {
+        span: '柚子',
+        img: '../../images/shop.jpg',
+        url: '../apple/apple'
+      },
+      {
+        span: '提子',
+        img: '../../images/shop.jpg',
+        url: '../apple/apple'
+      },
+      {
+        span: '芒果',
+        img: '../../images/shop.jpg',
+        url: '../apple/apple'
+      },
+      {
+        span: '榴莲',
+        img: '../../images/shop.jpg',
+        url: '../apple/apple'
+      },
+    ],
+    checkboxList: ['苹果', '香蕉', '橙子', '柚子'],
+    avatarUrl: './user-unlogin.png',
+    userInfo: {},
+    logged: false,
+    takeSession: false,
+    requestResult: ''
+  },
+
+  onLoad: function() {
+    if (!wx.cloud) {
+      wx.redirectTo({
+        url: '../chooseLib/chooseLib',
+      })
+      return
+    }
+
+    // 获取用户信息
+    wx.getSetting({
+      success: res => {
+        if (res.authSetting['scope.userInfo']) {
+          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
+          wx.getUserInfo({
+            success: res => {
+              this.setData({
+                avatarUrl: res.userInfo.avatarUrl,
+                userInfo: res.userInfo
+              })
+            }
+          })
+        }
+      }
+    })
+  },
+  end: function() {
+    console.log('hello leiyun')
+  },
+  change: function() {
+    // console.log(1)
+  },
+  checkboxChange: function(e) {
+    console.log(e.detail.value)
+  }
+
+  // onGetUserInfo: function(e) {
+  //   if (!this.logged && e.detail.userInfo) {
+  //     this.setData({
+  //       logged: true,
+  //       avatarUrl: e.detail.userInfo.avatarUrl,
+  //       userInfo: e.detail.userInfo
+  //     })
+  //   }
+  // },
+
+  // onGetOpenid: function() {
+  //   // 调用云函数
+  //   wx.cloud.callFunction({
+  //     name: 'login',
+  //     data: {},
+  //     success: res => {
+  //       console.log('[云函数] [login] user openid: ', res.result.openid)
+  //       app.globalData.openid = res.result.openid
+  //       wx.navigateTo({
+  //         url: '../userConsole/userConsole',
+  //       })
+  //     },
+  //     fail: err => {
+  //       console.error('[云函数] [login] 调用失败', err)
+  //       wx.navigateTo({
+  //         url: '../deployFunctions/deployFunctions',
+  //       })
+  //     }
+  //   })
+  // },
+
+  // 上传图片
+  // doUpload: function () {
+  //   // 选择图片
+  //   wx.chooseImage({
+  //     count: 1,
+  //     sizeType: ['compressed'],
+  //     sourceType: ['album', 'camera'],
+  //     success: function (res) {
+
+  //       wx.showLoading({
+  //         title: '上传中',
+  //       })
+
+  //       const filePath = res.tempFilePaths[0]
+        
+  //       // 上传图片
+  //       const cloudPath = 'my-image' + filePath.match(/\.[^.]+?$/)[0]
+  //       wx.cloud.uploadFile({
+  //         cloudPath,
+  //         filePath,
+  //         success: res => {
+  //           console.log('[上传文件] 成功：', res)
+
+  //           app.globalData.fileID = res.fileID
+  //           app.globalData.cloudPath = cloudPath
+  //           app.globalData.imagePath = filePath
+            
+  //           wx.navigateTo({
+  //             url: '../storageConsole/storageConsole'
+  //           })
+  //         },
+  //         fail: e => {
+  //           console.error('[上传文件] 失败：', e)
+  //           wx.showToast({
+  //             icon: 'none',
+  //             title: '上传失败',
+  //           })
+  //         },
+  //         complete: () => {
+  //           wx.hideLoading()
+  //         }
+  //       })
+
+  //     },
+  //     fail: e => {
+  //       console.error(e)
+  //     }
+  //   })
+  // },
+
+})
